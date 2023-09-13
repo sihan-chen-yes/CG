355b29df672dc0ebcfef05c7e3321c1d3ff1ca6b
Karlis Briedis <karlis.briedis@disneyresearch.com>
2023-09-13 18:13:57 +0200

    Adding support for headless and external testing

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,10 @@
 #include <nori/gui.h>
 #include <filesystem/path.h>
 
-int main(int argc, char **argv) {
-    using namespace nori;
+using namespace nori;
 
+
+bool run_gui(std::string filename, bool is_xml) {
     try {
         nanogui::init();
 
@@ -32,20 +33,13 @@ int main(int argc, char **argv) {
             // Scoped
             nanogui::ref<NoriScreen> screen = new NoriScreen(block);
 
-            // if file is passed as argument, handle it
-            if (argc == 2) {
-                std::string filename = argv[1];
-                filesystem::path path(filename);
-
-                if (path.extension() == "xml") {
+            if (filename.length() > 0) {
+                if (is_xml) {
                     /* Render the XML scene file */
                     screen->openXML(filename);
-                } else if (path.extension() == "exr") {
+                } else {
                     /* Alternatively, provide a basic OpenEXR image viewer */
                     screen->openEXR(filename);
-                } else {
-                    cerr << "Error: unknown file \"" << filename
-                    << "\", expected an extension of type .xml or .exr" << endl;
                 }
             }
 
@@ -56,7 +50,70 @@ int main(int argc, char **argv) {
 
     } catch (const std::exception &e) {
         cerr << "Fatal error: " << e.what() << endl;
-        return -1;
+        return 1;
     }
+
     return 0;
+}
+
+
+bool render_headless(std::string filename, bool is_xml) {
+    // TODOs - proper handling of an ctrl+z, progress bar, CL argument -b for headless
+	ImageBlock block(Vector2i(720, 720), nullptr);
+	RenderThread renderer(block);
+
+    if (!filename.length()) {
+        cerr << "Need to provide an input XML file to render in headless mode" << endl;
+        return 1;
+    }
+
+    if (!is_xml) {
+        cerr << "Only XML files supported in the headless mode" << endl;
+        return 1;
+    }
+
+	try {
+		// StringBar m_cliBar;
+		renderer.renderScene(filename);
+		while (renderer.isBusy()) {
+            std::cout << "rendering " << std::endl;
+			// cout << "\r" + m_cliBar.toString(renderer.getProgress(), 1.f);
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	}
+	catch (const std::exception &e) {
+		cerr << "Failed to render in the headless mode " << e.what() << endl;
+        return 1;
+	}
+
+    return 0;
+}
+
+
+int main(int argc, char **argv) {
+
+    // if file is passed as argument, handle it
+    std::string filename = "";
+    bool is_xml = false;
+    if (argc == 2) {
+        filename = argv[1];
+        filesystem::path path(filename);
+
+        if (path.extension() == "xml") {
+            is_xml = true;
+        } else if (path.extension() == "exr") {
+            /* Alternatively, provide a basic OpenEXR image viewer */
+            is_xml = false;
+        } else {
+            cerr << "Error: unknown file \"" << filename
+            << "\", expected an extension of type .xml or .exr" << endl;
+        }
+    }
+
+#ifndef NORI_HEADLESS
+    return run_gui(filename, is_xml);
+#else
+    return render_headless(filename, is_xml);
+#endif
+
 }
